35d7626499e7d6a7b81632ef9cf26d0275493a93
Tobias Fenster <tobias.fenster@cosmoconsult.com>
2021-04-12 22:04:15 +0000

    add headers and url functions

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
 
 var app = WebApplication.Create(args);
 
 app.MapGet("/", async http =>
 {
     await http.Response.WriteAsync("Hello World, v1!");
+});
+
+app.MapGet("/headers", async http =>
+{
+    var sb = new StringBuilder("Headers: \r\n");
+    foreach (var header in http.Request.Headers)
+    {
+        sb.Append($"{header.Key} = {header.Value}\r\n");
+    }
+    await http.Response.WriteAsync(sb.ToString());
+});
+
+app.MapGet("/url", async http =>
+{
+    await http.Response.WriteAsync(UriHelper.GetDisplayUrl(http.Request));
 });
 
 await app.RunAsync();
